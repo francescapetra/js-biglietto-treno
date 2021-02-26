a15a1f3fc7ae87fdb72fed3166a4df6259d44f94
francescapetra <francescalaurapetra@gmail.com>
2021-02-26 15:36:28 +0100

    3 condizioni

--- js/script.js
+++ js/script.js
@@ -8,3 +8,20 @@ console.log(eta);
 var costo = km * 0.21;
 document.getElementById('costo_biglietto').innerHTML = "Totale da pagare: " + "euro " + costo;
 // condizioni
+if ( eta <= 17 ) {
+  costo = ((costo *100) / 120);
+console.log (costo);
+document.getElementById('costo_biglietto').innerHTML = "Sconto Under applicato. Totale da pagare: " + "euro " + costo;
+document.getElementById('under').innerHTML = "Tariffa Under sconto 20%";
+document.getElementById('over').innerHTML = "";
+}else if (eta >= 65){
+  costo = ((costo *100) / 140);
+console.log (costo);
+document.getElementById('costo_biglietto').innerHTML = "Sconto Over applicato. Totale da pagare: " + "euro " + costo;
+document.getElementById('over').innerHTML = "Tariffa Over sconto 40%";
+document.getElementById('under').innerHTML = "";
+}else (eta >17<65)
+console.log (costo);
+document.getElementById('costo_biglietto').innerHTML = "Tariffa standard. Totale da pagare: " + "euro " + costo;
+document.getElementById('over').innerHTML = "";
+document.getElementById('under').innerHTML = "";
